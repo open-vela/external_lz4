e530200217278e38f999c0fbf49ab489bdc440d4
liujinye <liujinye@xiaomi.com>
2022-09-09 22:06:48 +0800

    Revert "external/lz4:LZ4 support on NUTTX"
    This reverts commit 28c14b69026384422422b20eb37a4dae616895f5.

--- tests/datagencli.c
+++ tests/datagencli.c
@@ -34,14 +34,6 @@
 
 
 /**************************************
-*  Compiler specific
-**************************************/
-#ifdef _MSC_VER    /* Visual Studio */
-#define strtoull    _strtoui64  /* https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/strtoui64-wcstoui64-strtoui64-l-wcstoui64-l */
-#endif
-
-
-/**************************************
 *  Constants
 **************************************/
 #define KB *(1 <<10)
@@ -111,7 +103,13 @@ int main(int argc, char** argv)
                     return usage(programName);
                 case 'g':
                     argument++;
-                    size = strtoull(argument, &argument, 10);
+                    size=0;
+                    while ((*argument>='0') && (*argument<='9'))
+                    {
+                        size *= 10;
+                        size += *argument - '0';
+                        argument++;
+                    }
                     if (*argument=='K') { size <<= 10; argument++; }
                     if (*argument=='M') { size <<= 20; argument++; }
                     if (*argument=='G') { size <<= 30; argument++; }
@@ -119,16 +117,35 @@ int main(int argc, char** argv)
                     break;
                 case 's':
                     argument++;
-                    seed = (U32) strtoul(argument, &argument, 10);
+                    seed=0;
+                    while ((*argument>='0') && (*argument<='9'))
+                    {
+                        seed *= 10;
+                        seed += *argument - '0';
+                        argument++;
+                    }
                     break;
                 case 'P':
                     argument++;
-                    proba = (double) strtoull(argument, &argument, 10);
+                    proba=0.0;
+                    while ((*argument>='0') && (*argument<='9'))
+                    {
+                        proba *= 10;
+                        proba += *argument - '0';
+                        argument++;
+                    }
+                    if (proba>100.) proba=100.;
                     proba /= 100.;
                     break;
                 case 'L':   /* hidden argument : Literal distribution probability */
                     argument++;
-                    litProba = (double) strtoull(argument, &argument, 10);
+                    litProba=0.;
+                    while ((*argument>='0') && (*argument<='9'))
+                    {
+                        litProba *= 10;
+                        litProba += *argument - '0';
+                        argument++;
+                    }
                     if (litProba>100.) litProba=100.;
                     litProba /= 100.;
                     break;
